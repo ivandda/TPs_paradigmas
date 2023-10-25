c4af9c1133c56cb300ea250a76cbe088b11503c1
ivandda <ivandominguezdealzaga@gmail.com>
2023-10-25 17:55:43 -0300

    add classes required in assigment

---
+++ TP4/linea/Linea.java
@@ -0,0 +1,24 @@
+package linea;
+
+public class Linea {
+    int base;
+    int height;
+    public Linea(int promptBase, int promptHeight, char c) {
+        this.base = promptBase;
+        this.height = promptHeight;
+    }
+
+    public boolean show() {
+        return false;
+    }
+
+    public boolean finished() {
+        return false;
+    }
+
+    public void playRedkAt(int prompt) {
+    }
+
+    public void playBlueAt(int prompt) {
+    }
+}
